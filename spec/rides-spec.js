35fa8e27b5139030bb0ee15f2e7c4203b35f51c8
Madeline Pickering <madelinepickering@gmail.com>
2016-02-24 13:25:19 -0700

    added two separate its

--- spec/rides-spec.js
+++ spec/rides-spec.js
@@ -1,7 +1,7 @@
 var rides = require("../lib/rides")
 
 describe("rides", function(){
-  it("testing",function(){
+  it("child is too short",function(){
     var child1 = {
       height: 3,
       name: "Julian",
@@ -25,7 +25,31 @@ describe("rides", function(){
     };
     expect(rides.isTallEnough(child1,ride1)).toEqual(true)
     expect(rides.isTallEnough(child2,ride1)).toEqual(true)
-    expect(rides.isTallEnough(child1,ride2)).toEqual(false)
     expect(rides.isTallEnough(child2,ride2)).toEqual(true)
   })
+  it("child is tall enough",function(){
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+
+    var ride1 = {
+    minHeight: 3,
+    maxHeight: 7
+    };
+
+    var ride2 = {
+    minHeight: 4,
+    maxHeight: 7
+    };
+    expect(rides.isTallEnough(child1,ride2)).toEqual(false)
+  })
+
 })
