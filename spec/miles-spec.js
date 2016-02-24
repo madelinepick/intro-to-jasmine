f5248b562502b4c76b6f6d991dfcfc17b4885a0a
Madeline Pickering <madelinepickering@gmail.com>
2016-02-24 11:52:38 -0700

    testing with true and false

--- spec/miles-spec.js
+++ spec/miles-spec.js
@@ -1,7 +1,20 @@
 var miles =  require('../lib/miles')
 
 describe("Miles", function(){
-  it("is our master plan here", function(){
-    expect(miles.someFunction()).toEqual("running stuff foreal")
-  })
-})
+  it("has enought gas to continue", function(){
+    var car = {
+      milesPerGallon: 20,
+      gallonsInTank: 3
+    };
+
+    var route = {
+      miles: 200,
+      destination: "Boulder",
+      location: "Fancy Land"
+    };
+    expect(miles.hasEnoughGas(car, route)).toEqual(false)
+    car.gallonsInTank += 10;
+    expect(miles.hasEnoughGas(car, route)).toEqual(true)
+
+  });
+});
